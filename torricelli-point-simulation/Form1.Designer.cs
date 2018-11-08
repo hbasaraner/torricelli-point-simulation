b8b982c5798961790952d3924c182f65be8063ee
Hayrettin Basaraner <hbasaraner@gmail.com>
2018-11-09 00:44:12 +0300

    Update

--- torricelli-point-simulation/Form1.Designer.cs
+++ torricelli-point-simulation/Form1.Designer.cs
@@ -30,30 +30,36 @@
         {
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.x1 = new System.Windows.Forms.TextBox();
-            this.x2 = new System.Windows.Forms.TextBox();
-            this.x3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.iterationCount = new System.Windows.Forms.TextBox();
+            this.btnSolve = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.isCircle = new System.Windows.Forms.RadioButton();
+            this.isRectangle = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.epsilon = new System.Windows.Forms.TextBox();
+            this.btnSimulate = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.y3 = new System.Windows.Forms.TextBox();
             this.y2 = new System.Windows.Forms.TextBox();
             this.y1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.epsilon = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.button2 = new System.Windows.Forms.Button();
+            this.x3 = new System.Windows.Forms.TextBox();
+            this.x2 = new System.Windows.Forms.TextBox();
+            this.x1 = new System.Windows.Forms.TextBox();
+            this.panelDraw = new System.Windows.Forms.Panel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // statusStrip1
             // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
             this.statusStrip1.Location = new System.Drawing.Point(0, 405);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.Size = new System.Drawing.Size(704, 22);
@@ -62,13 +68,15 @@
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.iterationCount);
+            this.panel1.Controls.Add(this.btnSolve);
             this.panel1.Controls.Add(this.panel3);
-            this.panel1.Controls.Add(this.radioButton2);
-            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.isCircle);
+            this.panel1.Controls.Add(this.isRectangle);
             this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.epsilon);
-            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btnSimulate);
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label3);
@@ -85,34 +93,139 @@
             this.panel1.Size = new System.Drawing.Size(295, 400);
             this.panel1.TabIndex = 1;
             // 
-            // panel2
+            // label7
             // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel2.Location = new System.Drawing.Point(301, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(400, 400);
-            this.panel2.TabIndex = 2;
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(127, 311);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Iteration Count";
             // 
-            // x1
+            // iterationCount
             // 
-            this.x1.Location = new System.Drawing.Point(58, 116);
-            this.x1.Name = "x1";
-            this.x1.Size = new System.Drawing.Size(47, 20);
-            this.x1.TabIndex = 0;
+            this.iterationCount.Location = new System.Drawing.Point(130, 331);
+            this.iterationCount.Name = "iterationCount";
+            this.iterationCount.Size = new System.Drawing.Size(152, 20);
+            this.iterationCount.TabIndex = 18;
             // 
-            // x2
+            // btnSolve
             // 
-            this.x2.Location = new System.Drawing.Point(58, 142);
-            this.x2.Name = "x2";
-            this.x2.Size = new System.Drawing.Size(47, 20);
-            this.x2.TabIndex = 1;
+            this.btnSolve.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnSolve.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSolve.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnSolve.Location = new System.Drawing.Point(130, 272);
+            this.btnSolve.Name = "btnSolve";
+            this.btnSolve.Size = new System.Drawing.Size(152, 24);
+            this.btnSolve.TabIndex = 17;
+            this.btnSolve.Text = "Solve";
+            this.btnSolve.UseVisualStyleBackColor = false;
+            this.btnSolve.Click += new System.EventHandler(this.Solve);
             // 
-            // x3
+            // panel3
             // 
-            this.x3.Location = new System.Drawing.Point(58, 168);
-            this.x3.Name = "x3";
-            this.x3.Size = new System.Drawing.Size(47, 20);
-            this.x3.TabIndex = 2;
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel3.Location = new System.Drawing.Point(15, 302);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(270, 3);
+            this.panel3.TabIndex = 16;
+            // 
+            // isCircle
+            // 
+            this.isCircle.AutoSize = true;
+            this.isCircle.Enabled = false;
+            this.isCircle.Location = new System.Drawing.Point(12, 334);
+            this.isCircle.Name = "isCircle";
+            this.isCircle.Size = new System.Drawing.Size(51, 17);
+            this.isCircle.TabIndex = 15;
+            this.isCircle.Text = "Circle";
+            this.isCircle.UseVisualStyleBackColor = true;
+            // 
+            // isRectangle
+            // 
+            this.isRectangle.AutoSize = true;
+            this.isRectangle.Checked = true;
+            this.isRectangle.Location = new System.Drawing.Point(12, 311);
+            this.isRectangle.Name = "isRectangle";
+            this.isRectangle.Size = new System.Drawing.Size(74, 17);
+            this.isRectangle.TabIndex = 14;
+            this.isRectangle.TabStop = true;
+            this.isRectangle.Text = "Rectangle";
+            this.isRectangle.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 278);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Epsilon";
+            // 
+            // epsilon
+            // 
+            this.epsilon.Location = new System.Drawing.Point(59, 275);
+            this.epsilon.Name = "epsilon";
+            this.epsilon.Size = new System.Drawing.Size(65, 20);
+            this.epsilon.TabIndex = 12;
+            // 
+            // btnSimulate
+            // 
+            this.btnSimulate.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnSimulate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSimulate.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnSimulate.Location = new System.Drawing.Point(12, 357);
+            this.btnSimulate.Name = "btnSimulate";
+            this.btnSimulate.Size = new System.Drawing.Size(270, 40);
+            this.btnSimulate.TabIndex = 11;
+            this.btnSimulate.Text = "Simulate";
+            this.btnSimulate.UseVisualStyleBackColor = false;
+            this.btnSimulate.Click += new System.EventHandler(this.Simulate);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 171);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Point 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Point 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Point 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(127, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Y";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "X";
             // 
             // y3
             // 
@@ -135,133 +248,53 @@
             this.y1.Size = new System.Drawing.Size(47, 20);
             this.y1.TabIndex = 3;
             // 
-            // label1
+            // x3
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(77, 100);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(14, 13);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "X";
+            this.x3.Location = new System.Drawing.Point(58, 168);
+            this.x3.Name = "x3";
+            this.x3.Size = new System.Drawing.Size(47, 20);
+            this.x3.TabIndex = 2;
             // 
-            // label2
+            // x2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(127, 100);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(14, 13);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Y";
+            this.x2.Location = new System.Drawing.Point(58, 142);
+            this.x2.Name = "x2";
+            this.x2.Size = new System.Drawing.Size(47, 20);
+            this.x2.TabIndex = 1;
             // 
-            // label3
+            // x1
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 119);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(40, 13);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Point 1";
+            this.x1.Location = new System.Drawing.Point(58, 116);
+            this.x1.Name = "x1";
+            this.x1.Size = new System.Drawing.Size(47, 20);
+            this.x1.TabIndex = 0;
             // 
-            // label4
+            // panelDraw
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 145);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(40, 13);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Point 2";
+            this.panelDraw.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panelDraw.Location = new System.Drawing.Point(301, 0);
+            this.panelDraw.Name = "panelDraw";
+            this.panelDraw.Size = new System.Drawing.Size(400, 400);
+            this.panelDraw.TabIndex = 2;
             // 
-            // label5
+            // toolStripStatusLabel1
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 171);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(40, 13);
-            this.label5.TabIndex = 10;
-            this.label5.Text = "Point 3";
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.button1.Location = new System.Drawing.Point(97, 347);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(185, 40);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "Simulate";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.Simulate);
-            // 
-            // epsilon
-            // 
-            this.epsilon.Location = new System.Drawing.Point(58, 292);
-            this.epsilon.Name = "epsilon";
-            this.epsilon.Size = new System.Drawing.Size(47, 20);
-            this.epsilon.TabIndex = 12;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 295);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(41, 13);
-            this.label6.TabIndex = 13;
-            this.label6.Text = "Epsilon";
-            // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(12, 347);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(74, 17);
-            this.radioButton1.TabIndex = 14;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Rectangle";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(12, 370);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(51, 17);
-            this.radioButton2.TabIndex = 15;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Circle";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.panel3.Location = new System.Drawing.Point(12, 322);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(270, 3);
-            this.panel3.TabIndex = 16;
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.button2.Location = new System.Drawing.Point(111, 288);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(171, 24);
-            this.button2.TabIndex = 17;
-            this.button2.Text = "Solve";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.Solve);
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(10, 17);
+            this.toolStripStatusLabel1.Text = " ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(704, 427);
-            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panelDraw);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.statusStrip1);
             this.Name = "Form1";
             this.Text = "Torricelli Point Simulation";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -273,13 +306,13 @@
 
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnSolve;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton isCircle;
+        private System.Windows.Forms.RadioButton isRectangle;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox epsilon;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSimulate;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
@@ -291,7 +324,10 @@
         private System.Windows.Forms.TextBox x3;
         private System.Windows.Forms.TextBox x2;
         private System.Windows.Forms.TextBox x1;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panelDraw;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox iterationCount;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
     }
 }
 
--- torricelli-point-simulation/Form1.cs
+++ torricelli-point-simulation/Form1.cs
@@ -13,13 +13,17 @@ namespace torricelli_point_simulation
 {
     public partial class Form1 : Form
     {
+        public Graphics Graphics;
 
-        public double[] x = new double[3];
-        public double[] y = new double[3];
+        public int[] x = new int[3];
+        public int[] y = new int[3];
 
         public Form1()
         {
+            CheckForIllegalCrossThreadCalls = false;
             InitializeComponent();
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint | ControlStyles.DoubleBuffer, true);
+            this.ResizeRedraw = true;
         }
 
         private void Solve(object sender, EventArgs e)
@@ -28,8 +32,8 @@ namespace torricelli_point_simulation
             double distance, minDistance = double.MaxValue, minDistanceX = double.MaxValue, minDistanceY = double.MaxValue;
             try
             {
-                x[0] = Convert.ToDouble(x1.Text); x[1] = Convert.ToDouble(x2.Text); x[2] = Convert.ToDouble(x3.Text);
-                y[0] = Convert.ToDouble(y1.Text); y[1] = Convert.ToDouble(y2.Text); y[2] = Convert.ToDouble(y3.Text);
+                x[0] = Convert.ToInt32(x1.Text); x[1] = Convert.ToInt32(x2.Text); x[2] = Convert.ToInt32(x3.Text);
+                y[0] = Convert.ToInt32(y1.Text); y[1] = Convert.ToInt32(y2.Text); y[2] = Convert.ToInt32(y3.Text);
                 eps = Convert.ToDouble(epsilon.Text);
 
                 minX = maxX = x[0];
@@ -71,7 +75,94 @@ namespace torricelli_point_simulation
 
         private void Simulate(object sender, EventArgs e)
         {
+            Graphics g = panelDraw.CreateGraphics();
+            Pen pRectangle = new Pen(Color.Blue, 5);
+            Pen pCircle = new Pen(Color.Magenta, 5);
+            Pen pTriangle = new Pen(Color.Orange, 2);
+            Pen pLine = new Pen(Color.Black, 2);
+            SolidBrush bCircle = new SolidBrush(Color.Magenta);
 
+            // Simulation with rectangle approach
+            if (isRectangle.Checked)
+            {
+                double x_approach = 0, y_approach = 0, maxX, minX, maxY, minY, distance = 0, minDistance = double.MaxValue, minDistanceX = double.MaxValue, minDistanceY = double.MaxValue;
+                try
+                {
+
+                    Random r = new Random();
+
+                    #region Get coordinates
+                    x[0] = Convert.ToInt32(x1.Text); x[1] = Convert.ToInt32(x2.Text); x[2] = Convert.ToInt32(x3.Text);
+                    y[0] = Convert.ToInt32(y1.Text); y[1] = Convert.ToInt32(y2.Text); y[2] = Convert.ToInt32(y3.Text);
+
+                    int p_const = panelDraw.Width / 2;
+
+                    Point[] points = { new Point(x[0] + p_const, y[0] + p_const - 2 * y[0]),
+                                       new Point(x[1] + p_const, y[1] + p_const - 2 * y[1]),
+                                       new Point(x[2] + p_const, y[2] + p_const - 2 * y[2]) };
+
+                    g.DrawLine(pLine, 0, p_const, panelDraw.Width, p_const);
+                    g.DrawLine(pLine, p_const, 0, p_const, panelDraw.Width);
+                    g.DrawPolygon(pTriangle, points);
+                    #endregion
+
+
+
+                    #region Find max and min values
+                    minX = maxX = x[0];
+                    minY = maxY = y[0];
+
+                    for (int i = 1; i < 3; i++)
+                    {
+                        if (x[i] < minX) minX = x[i];
+                        if (x[i] > maxX) maxX = x[i];
+                        if (y[i] < minY) minY = y[i];
+                        if (y[i] > maxY) maxY = y[i];
+                    }
+                    #endregion
+
+                    int count = Convert.ToInt32(iterationCount.Text);
+                    for (int j = 1; j <= count; j++)
+                    {
+                        x_approach = r.Next(0, int.MaxValue) / (double)int.MaxValue * (maxX - minX) + minX;
+                        y_approach = r.Next(0, int.MaxValue) / (double)int.MaxValue * (maxY - minY) + minY;
+
+
+                        g.FillEllipse(bCircle, (float)x_approach + p_const, (float)y_approach + p_const - (2 * (float)y_approach), 1, 1);
+                        g.DrawPolygon(pTriangle, points);
+
+                        distance = 0;
+                        for (int i = 0; i < 3; i++)
+                        {
+                            distance += Math.Sqrt(Math.Pow(x_approach - x[i], 2) + (Math.Pow(y_approach - y[i], 2)));
+                        }
+                        if (distance < minDistance)
+                        {
+                            minDistance = distance;
+                            minDistanceX = x_approach;
+                            minDistanceY = y_approach;
+                        }
+
+                        if (j % 1000 == 0)
+                        {
+                            toolStripStatusLabel1.Text = "Distance: " + minDistance.ToString();
+                            statusStrip1.Update();
+                        }
+                            
+                    }
+                    toolStripStatusLabel1.Text = "OK! Distance: " + minDistance.ToString() + " | Min. Distance of X: " + x_approach.ToString() + " | Min. Distance of Y: " + y_approach.ToString();
+                }
+                catch
+                {
+
+                }
+            }
+
+            // Simulation with circle approach
+            else if (isCircle.Checked)
+            {
+
+            }
         }
     }
 }
